6319e67565a8af62162c39bab9229fd2584bcf0d
Joel Mero <alejomero1@gmail.com>
2021-09-06 19:59:35 -0500

    Variables Locales y Globales

---
+++ src/main/java1/ejercicios/variables/locales/globales/Jugador.java
@@ -0,0 +1,17 @@
+package ejercicios.variables.locales.globales;
+
+public class Jugador {
+
+	//Globales
+	private String nombre;
+	private String apellido;
+	private int edad;
+	
+	//Locales 
+	public void jugar() {
+		String nombreJuego;
+		String añoLanzamientoJuego;
+		String nombreCreador;
+		
+	}
+}
